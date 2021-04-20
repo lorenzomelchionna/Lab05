5b79753e6fc1a9c5e38ba795ff34466ad678e03a
User <User@DESKTOP-1JG37L3>
2021-04-20 10:20:20 +0200

    Aggiunta Data Source

--- Lab05_Anagrammi/src/main/java/it/polito/tdp/anagrammi/db/DBConnect.java
+++ Lab05_Anagrammi/src/main/java/it/polito/tdp/anagrammi/db/DBConnect.java
@@ -4,15 +4,29 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
+import com.zaxxer.hikari.HikariDataSource;
+
 public class DBConnect {
 
 	static private final String jdbcUrl = "jdbc:mysql://localhost/dizionario?user=root&password=w la patata";
-
+	
+	static private HikariDataSource ds = null;
+	
 	public static Connection getConnection() {
-
+		
+		if(ds==null) {
+			
+			ds = new HikariDataSource();
+			
+			ds.setJdbcUrl(jdbcUrl);
+			ds.setUsername("root");
+			ds.setPassword("w la patata");
+			
+		}
+		
 		try {
 			
-				Connection connection = DriverManager.getConnection(jdbcUrl);
+				Connection connection = ds.getConnection();
 				return connection;
 
 		} catch (SQLException e) {
